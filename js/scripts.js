9769e8a008179d841bbc99508b30da6d82ef7897
Stacy Kiriiri <Wkiriiri@gmail.com>
2020-04-12 01:16:48 +0300

    adds constructors to my js file

---
+++ js/scripts.js
@@ -0,0 +1,28 @@
+function Order(sizes,crusts,toppings) {
+    this.sizes=sizes;
+    this.crusts=crusts;
+    this.toppings=toppings;
+}
+
+var sizes=new Order();
+var crusts=new Order();
+var toppings=new Order();
+
+function Price(small,medium,large) {
+    this.small=small;
+    this.medium=medium;
+    this.large=large;
+}
+var small=new Price(500);
+var medium=new Price(800);
+var large=new Price(1000);
+
+function Crust(prices) {
+    this.prices=price;
+}
+
+var smallCrust=new Crust(200);
+var mediumCrust=new Crust(300);
+var largeCrust=new Crust(400);
+
+function Total()
