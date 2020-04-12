4b78200afed34520b3cf3da54a0768c75b434dc4
Stacy Kiriiri <Wkiriiri@gmail.com>
2020-04-13 00:02:45 +0300

    assigns identifiers for sizes,crusts and toppings

--- js/scripts.js
+++ js/scripts.js
@@ -1,44 +1,48 @@
 document.addEventListener('DOMContentLoaded',function () {
+    document.getElementById("submit").addEventListener("click", function(event) {
+        event.preventDefault()
+    })
+});
     var pizzaSizes=document.getElementById("sizes");
     var pizzaCrusts=document.getElementById("crusts");
     var pizzaToppings=document.getElementById("toppings");
 
     var sizes=["small","medium","large"];
-    var crusts=["Stuffed","Cheese","Cauliflower","Gluten-free","Boboli","Neapolitan","Silican","Vegan"];
-    var toppings=["Pepperonni","Mushroom","Onions","Sausages","Bacon","Pineapple","Black olives","Green pepper"];
+    var crusts=["Stuffed", "Cheese", "Cauliflower", "Gluten-free", "Boboli", "Neapolitan", "Silican", "Vegan"];
+    var toppings=["Pepperonni", "Mushroom", "Onions", "Sausages", "Bacon", "Pineapple", "Black olives","Green pepper"];
 
     var crustBasePrice=100;
     var toppingsBasePrice=200;
 
-    for(var i=0; i<sizes.length;i++) {
+    for(var i=0;i<sizes.length;i++) {
         var size=sizes[i];
         var el=document.createElement("option");
         el.textContent=size;
         el.value=i;
         pizzaSizes.appendChild(el);
     }
-    for(var i=0; i<crusts.length; i++) {
+    for(var i=0;i<crusts.length;i++) {
         var crust=crusts[i];
         var el=document.createElement("option");
         el.textContent=crust;
         el.value=crustBasePrice;
         pizzaCrusts.appendChild(el);
     }
-    for(var i=0; i<toppings.length;i++) {
+    for(var i=0;i<toppings.length;i++) {
         var topping=toppings[i];
         var el=document.createElement("option");
         el.textContent=topping;
         el.value=toppingsBasePrice;
         pizzaToppings.appendChild(el);
-    }    
-});
+    }   
+
 var pizzaBasePrice=500;
 
 function SubmitData () {
     var selectedPizzaSize=parseInt(document.getElementById("sizes").value);
     var selectedSize=$("#sizes option:selected").text();
-    var selectedCrustPrice=parseInt(document.getElementById("crusts").value);
-    var selectedCrust=$("crusts option:selected").text();
+    var selectedCrustsPrice=parseInt(document.getElementById("crusts").value);
+    var selectedCrust=$("#crusts option:selected").text();
     var selectedToppingPrice=parseInt(document.getElementById("toppings").value);
     var selectedTopping=$("#toppings option:selected").text();
     var numberOfPizzas=parseInt(document.getElementById("number-of-pizzas").value);
@@ -54,7 +58,7 @@ function SubmitData () {
         selectedToppingPrice=selectedToppingPrice + increasePrice(selectedToppingPrice,20)
     }
 
-    var order=new order(selectedSize, selectedCrust,selectedCrustPrice,selectedToppingPrice,selectedTopping,numberOfPizzas);
+    var order=new Order(selectedSize, selectedCrust,selectedCrustsPrice,selectedToppingPrice,selectedTopping,numberOfPizzas);
 
     console.log(order)
 }
